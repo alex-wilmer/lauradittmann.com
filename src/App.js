e7952cd333549ecde2e715f4097fb69d4f8caf34
Alex Wilmer <djazium@gmail.com>
2018-06-02 18:21:40 -0400

    💄 images and stuff

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import StackGrid from "react-stack-grid";
+import range from "lodash.range";
+
+const width = 200;
+const images = 16;
 
 class App extends Component {
+  state = { fade: false };
+  componentDidMount() {
+    setTimeout(() => this.setState({ fade: true }), 1000);
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className={`bg ${this.state.fade ? "fade" : ""}`}>
+        <div className={`logo ${this.state.fade ? "fade" : ""}`}>
+          <div className="name">
+            <img
+              style={{ width: 100 }}
+              src={process.env.PUBLIC_URL + "logo.png"}
+            />
+          </div>
+        </div>
+        <div className={`grid-cnt ${this.state.fade ? "fade" : ""}`}>
+          <StackGrid
+            gutterWidth={-1}
+            gutterHeight={-5}
+            columnWidth={width}
+            monitorImagesLoaded
+          >
+            {range(0, 40).map(i => (
+              <div key={i}>
+                <img
+                  style={{ width, height: width }}
+                  src={
+                    process.env.PUBLIC_URL +
+                    "tile" +
+                    ((i % images) + 1) +
+                    ".png"
+                  }
+                />
+              </div>
+            ))}
+          </StackGrid>
+        </div>
       </div>
     );
   }
